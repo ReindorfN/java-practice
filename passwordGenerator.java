e04e1e09a2c37b90f0b6cc21e88920c5363f5ca2
ReindorfN <reindorfnarh@gmail.com>
2024-03-10 19:54:18 +0000

    Password Generator
    This program generates random paswords.

---
+++ passwordGenerator.java
@@ -0,0 +1,17 @@
+//Imports
+import java.util.Scanner;
+import java.util.Random;
+
+public class passwordGenerator {
+	
+	static String generatePassword(int passwordLenght, int uppercaseChars, int lowercaseChars, 
+	int numbers, specialChars){
+		
+	}
+	
+	public static void main(String[] args){
+	Scanner user_input = new Scanner(System.in);
+	System.out.println("This is a password genetator.");
+	
+	}
+}
