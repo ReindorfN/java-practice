237b6743a83230e79464da495644b31830a9d9b4
ReindorfN <reindorfnarh@gmail.com>
2024-03-11 20:45:48 +0000

    For loops
    This is a practice on for loops

---
+++ fortest.java
@@ -0,0 +1,13 @@
+/*
+Demonstrate the for loop
+call this file "ForTest.java
+*/
+
+class ForTest{
+	public static void main (String[] arhs){
+		int x;
+		for (x = 0;x<10; x = x+1;)
+			System.out.println("This is x: " +x);
+		System.out.println("This is x: " +x);
+	}
+}
